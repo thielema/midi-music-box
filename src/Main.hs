9728823b22bd689dc2721b19d79ffcc779b5154d
haskell <haskell@henning-thielemann.de>
2015-05-17 20:18:34 +0000

    place dots according to MIDI file input

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,20 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
 module Main where
 
+import qualified Sound.MIDI.Message.Class.Query as Query
+
+import qualified Sound.MIDI.Message.Channel.Voice as VoiceMsg
+import qualified Sound.MIDI.File.Event as FileEvent
+import qualified Sound.MIDI.File.Load as Load
+import qualified Sound.MIDI.File as MidiFile
+
+import qualified Data.EventList.Absolute.TimeBody as AbsEventList
+import qualified Data.EventList.Relative.TimeBody as EventList
+
 import qualified Diagrams.Backend.Postscript.CmdLine as PS
 import Diagrams.Prelude
+
+import Data.Tuple.HT (swap, )
 
 
 numLong, numAcross :: Int
@@ -39,20 +51,39 @@ across =
       let len = horlen (numLong-1)
       in  [hrule len, hrule len # lc grey]
 
-dots :: Diagram PS.B
-dots =
-   foldr1 atop $
-   map
-      (\(x,y) -> translate (r2 (horlen x, - verlen y)) $ fc blue $ circle 0.05)
-      [(0,0), (1,1), (3,2), (3,4)]
-
-diag :: Diagram PS.B
-diag =
-   dots `atop`
+grid :: Diagram PS.B
+grid =
    alignTL long `atop` alignTL across `atop`
    translateY 0.15 labels `atop`
    (translateX (-0.1) $ alignBL $ lc white $
     rect (horlen numLong) (1.5*horsep))
+
+
+dots :: [(Int, Double)] -> Diagram PS.B
+dots poss =
+   foldr1 atop $
+   flip map poss $
+   \(x,y) ->
+      translate (r2 (horlen x, - versep * y)) $
+      lw veryThin $ fc blue $ circle (horsep/4)
+
+layoutDots :: MidiFile.T -> [(Int, Double)]
+layoutDots (MidiFile.Cons typ division tracks) =
+   map swap $ AbsEventList.toPairList $
+   AbsEventList.mapTime ((/0.1) . realToFrac) $
+   AbsEventList.mapMaybe
+      (\fev -> do
+         FileEvent.MIDIEvent ev <- Just fev
+         (_c, (_v, p, True)) <- Query.noteExplicitOff ev
+         Just $ VoiceMsg.subtractPitch (VoiceMsg.toPitch 60) p) $
+   EventList.toAbsoluteEventList 0 $
+   MidiFile.mergeTracks typ $
+   map (MidiFile.secondsFromTicks division) tracks
+
+diag :: FilePath -> IO (Diagram PS.B)
+diag path = do
+   midi <- Load.fromFile path
+   return $ dots (layoutDots midi) `atop` grid
 
 main :: IO ()
 main = PS.mainWith diag
