8bb627dd72d0fe73f3da11d5dec15960727bc5fa
haskell <haskell@henning-thielemann.de>
2015-05-17 11:00:40 +0000

    horlen, verlen: avoid frequent use of fromIntegral

--- src/Main.hs
+++ src/Main.hs
@@ -13,6 +13,10 @@ horsep, versep :: Double
 horsep = 0.2
 versep = 0.3
 
+horlen, verlen :: Int -> Double
+horlen n = fromIntegral n * horsep
+verlen n = fromIntegral n * versep
+
 
 labels :: Diagram PS.B
 labels =
@@ -24,7 +28,7 @@ long :: Diagram PS.B
 long =
    hcat' (with & sep .~ horsep) $
    map (\(wid, col) ->
-          vrule ((fromIntegral numAcross-1) * versep) # lw wid # lc col) $
+          vrule (verlen (numAcross-1)) # lw wid # lc col) $
       let l = (thin, black); h = (thick, darkgreen)
       in  [l, l, h, l, h, l, h, l, h, l, h, l, l, l, l]
 
@@ -32,14 +36,14 @@ across :: Diagram PS.B
 across =
    vcat' (with & sep .~ versep) $
    take numAcross $ cycle $
-      let len = (fromIntegral numLong-1) * horsep
+      let len = horlen (numLong-1)
       in  [hrule len, hrule len # lc grey]
 
 dots :: Diagram PS.B
 dots =
    foldr1 atop $
    map
-      (\(x,y) -> translate (r2 (x*horsep, -y*versep)) $ fc blue $ circle 0.05)
+      (\(x,y) -> translate (r2 (horlen x, - verlen y)) $ fc blue $ circle 0.05)
       [(0,0), (1,1), (3,2), (3,4)]
 
 diag :: Diagram PS.B
@@ -48,7 +52,7 @@ diag =
    alignTL long `atop` alignTL across `atop`
    translateY 0.15 labels `atop`
    (translateX (-0.1) $ alignBL $ lc white $
-    rect (fromIntegral numLong * horsep) (1.5*horsep))
+    rect (horlen numLong) (1.5*horsep))
 
 main :: IO ()
 main = PS.mainWith diag
