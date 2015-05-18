344c6af4af5c577137e976fd8470d9f0d2ca06d5
haskell <haskell@henning-thielemann.de>
2015-05-18 09:25:09 +0000

    use coordinates and line widths that do not depend on image size

--- src/Main.hs
+++ src/Main.hs
@@ -26,26 +26,30 @@ numLong :: Int
 numLong = 15
 
 horsep, versep :: Double
-horsep = 0.2
-versep = 0.3
+horsep = 40
+versep = 60
 
 horlen, verlen :: Int -> Double
 horlen n = fromIntegral n * horsep
 verlen n = fromIntegral n * versep
 
+normalLW, thickLW :: Diag -> Diag
+normalLW = lwO 5
+thickLW = lwO 8
+
 
 labels :: Diag
 labels =
    hcat' (with & sep .~ horsep) $
-   map (\str -> text str # fontSizeL 0.2) $
+   map (\str -> text str # fontSizeL horsep) $
    map (:[]) "CDEFGABCDEFGABC"
 
 long :: Int -> Diag
 long numIntervals =
    hcat' (with & sep .~ horsep) $
    map (\(wid, col) ->
-          vrule (verlen numIntervals) # lw wid # lc col) $
-      let l = (thin, black); h = (thick, darkgreen)
+          vrule (verlen numIntervals) # wid # lc col) $
+      let l = (normalLW, black); h = (thickLW, darkgreen)
       in  [l, l, h, l, h, l, h, l, h, l, h, l, l, l, l]
 
 across :: Int -> Diag
@@ -53,14 +57,14 @@ across numIntervals =
    vcat' (with & sep .~ versep) $
    take (succ numIntervals) $ cycle $
       let len = horlen (numLong-1)
-      in  [hrule len, hrule len # lc grey]
+      in  map (# normalLW) [hrule len, hrule len # lc grey]
 
 grid :: Int -> Diag
 grid numIntervals =
    alignTL (long numIntervals) `atop`
    alignTL (across numIntervals) `atop`
-   translateY 0.15 labels `atop`
-   (translateX (-0.1) $ alignBL $ lc white $
+   translateY (0.5*versep) labels `atop`
+   (translateX (-0.5*horsep) $ alignBL $ lc white $
     rect (horlen numLong) (1.5*horsep))
 
 
@@ -70,7 +74,7 @@ dots poss =
    flip map poss $
    \(x,y) ->
       translate (r2 (horlen x, - versep * y)) $
-      lw veryThin $ fc blue $ circle (horsep/4)
+      normalLW $ fc blue $ circle (horsep/4)
 
 layoutDots :: MidiFile.T -> [(Int, Double)]
 layoutDots (MidiFile.Cons typ division tracks) =
