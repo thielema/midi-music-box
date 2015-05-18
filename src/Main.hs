4357de39b0fd4855736fd87922193c87a5cb1364
haskell <haskell@henning-thielemann.de>
2015-05-18 10:57:20 +0000

    correctly map MIDI notes to note lines
    special graphics for semitones

--- src/Main.hs
+++ src/Main.hs
@@ -18,9 +18,12 @@ import qualified System.IO as IO
 import Text.Printf (hPrintf, )
 
 import Control.Monad (when, )
+
+import qualified Data.Map as Map; import Data.Map (Map, )
+import qualified Data.NonEmpty as NonEmpty
 import Data.Foldable (foldMap, )
 import Data.List.HT (partition, )
-import Data.Tuple.HT (swap, mapSnd, )
+import Data.Tuple.HT (mapSnd, )
 
 
 type Diag = Diagram PS.B
@@ -74,22 +77,36 @@ grid numIntervals =
     rect (horlen numLong) (1.5*horsep))
 
 
-dots :: [(Int, Double)] -> Diag
+dots :: [((Int, Double), Bool)] -> Diag
 dots poss =
    flip foldMap poss $
-   \(x,y) ->
+   \((x,y),whole) ->
       translate (r2 (horlen x, - versep * y)) $
-      normalLW $ fc blue $ circle (horsep/4)
+      if whole
+        then normalLW $ fc blue $ circle (horsep*0.25)
+        else (fontSizeL horsep $ text "#") <>
+             (lwO 0 $ fc yellow $ circle (horsep*0.4))
 
-layoutDots :: MidiFile.T -> [(Int, Double)]
+noteMap :: Map Int (Int, Bool)
+noteMap =
+   let o = True; x = False
+       semitones = cycle [o,x,o,x,o,o,x,o,x,o,x,o]
+   in  Map.fromList $ take 25 $ zip [0..] $
+       zip
+          (NonEmpty.tail $ NonEmpty.scanl (+) (-1) $ map fromEnum semitones)
+          semitones
+
+layoutDots :: MidiFile.T -> [((Int, Double), Bool)]
 layoutDots (MidiFile.Cons typ division tracks) =
-   map swap $ AbsEventList.toPairList $
+   map (\(t,(p,whole)) -> ((p,t), whole)) $
+   AbsEventList.toPairList $
    AbsEventList.mapTime ((/timeStep) . realToFrac) $
    AbsEventList.mapMaybe
       (\fev -> do
          FileEvent.MIDIEvent ev <- Just fev
          (_c, (_v, p, True)) <- Query.noteExplicitOff ev
-         Just $ VoiceMsg.subtractPitch (VoiceMsg.toPitch 60) p) $
+         flip Map.lookup noteMap $
+            VoiceMsg.subtractPitch (VoiceMsg.toPitch 60) p) $
    EventList.toAbsoluteEventList 0 $
    MidiFile.mergeTracks typ $
    map (MidiFile.secondsFromTicks division) tracks
@@ -99,12 +116,13 @@ diag path = do
    midi <- Load.fromFile path
    let cloud = layoutDots midi
        (tooSmall, (fitting, tooBig)) =
-         mapSnd (partition ((<=numLong) . fst)) $ partition ((<0) . fst) cloud
+         mapSnd (partition ((<=numLong) . fst . fst)) $
+         partition ((<0) . fst . fst) cloud
    when (not $ null tooSmall) $
       hPrintf IO.stderr "Warning: %i notes are too low\n" $ length tooSmall
    when (not $ null tooBig) $
       hPrintf IO.stderr "Warning: %i notes are too high\n" $ length tooBig
-   return $ dots fitting `atop` (grid $ ceiling $ maximum $ map snd cloud)
+   return $ dots fitting `atop` (grid $ ceiling $ maximum $ map (snd . fst) cloud)
 
 main :: IO ()
 main = PS.mainWith diag
